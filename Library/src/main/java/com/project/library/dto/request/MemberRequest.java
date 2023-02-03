01c5a07f82f2e1ff53166c2f077684f1226f7bf8
JHK922 <apfhdxc@gmail.com>
2023-02-04 00:31:23 +0900

    MemberRequest, MemberResponse dto 생성 ver1

---
+++ Library/src/main/java/com/project/library/dto/request/MemberRequest.java
@@ -0,0 +1,26 @@
+package com.project.library.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 회원정보 생성, 수정에 사용 될 요청 클래스
+ */
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberRequest {
+
+    private long id;            // PK
+    private String email;       // 이메일 (로그인 아이디)
+    private String password1;   // 비밀번호
+    private String password2;   // 비밀번호 확인
+    private String name;        // 이름
+    private int birth;          // 생년월일
+    private String address;     // 주소
+    private String phone;       // 핸드폰번호
+    private Date createAt;      // 가입날짜
+}
