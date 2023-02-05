d6ba52071b4d3f36b0b20e9f6057ba3fd0900684
ClimberCode <kcjckswnd@gamil.com>
2023-02-05 15:05:14 +0900

    출판일 변수명 수정

--- Library/src/main/java/com/project/library/dto/request/BookPostRequestDto.java
+++ Library/src/main/java/com/project/library/dto/request/BookPostRequestDto.java
@@ -14,7 +14,7 @@ public class BookPostRequestDto {
     private Long id;
     private String title; //책 제목
     private String writer; //책 지은이
-    private Date p_date; //책 출판일
+    private Date publicateAt; //책 출판일
     private String status; //책 상태(대여 유/무, 입고 예정 등)
 
 }
