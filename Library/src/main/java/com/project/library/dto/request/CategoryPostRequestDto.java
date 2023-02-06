597c2f683d03cbc5151a72a6e666a8b560700e3f
ClimberCode <kcjckswnd@gamil.com>
2023-02-06 19:27:21 +0900

    categoryCreate : PostDto, ResponseDto 생성

---
+++ Library/src/main/java/com/project/library/dto/request/CategoryPostRequestDto.java
@@ -0,0 +1,10 @@
+package com.project.library.dto.request;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class CategoryPostRequestDto {
+    private Long id;
+    private String name;
+}
