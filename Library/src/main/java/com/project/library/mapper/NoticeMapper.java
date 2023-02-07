d4f7f2c55b8f6a1dae321f3fae68e5850d7eefd3
JHK922 <apfhdxc@gmail.com>
2023-02-07 22:11:39 +0900

    NoticeMapper, xml 파일 게시글 전체조회, 지정조회 기능 추가

--- Library/src/main/java/com/project/library/mapper/NoticeMapper.java
+++ Library/src/main/java/com/project/library/mapper/NoticeMapper.java
@@ -1,7 +1,10 @@
 package com.project.library.mapper;
 
 import com.project.library.dto.request.NoticeRequest;
+import com.project.library.dto.response.NoticeResponse;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface NoticeMapper {
@@ -11,5 +14,27 @@ public interface NoticeMapper {
      * @param request
      */
     void saveNotice(NoticeRequest request);
+
+    /**
+     * 전체 게시글 조회
+     * @return
+     */
+    List<NoticeResponse> findAll();
+
+    /**
+     * 해당 게시물 조회
+     * @param id - Pk
+     * @return
+     */
+    NoticeResponse findById(Long id);
+
+    /**
+     * 해당 게시물 조회
+     * @param memberId - FK
+     * @return
+     */
+    NoticeResponse findByMemberId(Long memberId);
+
+
 
 }
