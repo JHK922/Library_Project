a0bbebf73710436253db2e9be6d4418c96189340
JHK922 <apfhdxc@gmail.com>
2023-02-07 23:46:02 +0900

    NoticeRequest, Response deleteYN 필드 추가

--- Library/src/main/java/com/project/library/dto/response/NoticeResponse.java
+++ Library/src/main/java/com/project/library/dto/response/NoticeResponse.java
@@ -6,10 +6,11 @@ import java.time.LocalDateTime;
 @Data
 public class NoticeResponse {
 
-    private long id;                  // PK
-    private String title;             // 제목
-    private String content;           // 내용
-    private LocalDateTime createdAt;   // 작성일
-    private LocalDateTime modifiedAt; // 수정일
-    private long memberId;            // FK
+    private long id;                    // PK
+    private String title;               // 제목
+    private String content;             // 내용
+    private LocalDateTime createdAt;    // 작성일
+    private LocalDateTime modifiedAt;   // 수정일
+    private boolean deleteYN;           // 삭제 여부
+    private long memberId;              // FK
 }
