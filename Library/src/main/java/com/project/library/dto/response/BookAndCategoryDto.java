9ae3013d75ea64e181444673e67ec8a1f11e1e53
ClimberCode <kcjckswnd@gamil.com>
2023-02-10 21:18:03 +0900

    createBook-Refactor : Book, Category 사이의 중간 테이블용 DTO 생성

---
+++ Library/src/main/java/com/project/library/dto/response/BookAndCategoryDto.java
@@ -0,0 +1,13 @@
+package com.project.library.dto.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class BookAndCategoryDto {
+    private Long bookId;
+    private Integer categoryId;
+
+}
