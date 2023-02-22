10f7ad1023af814d045600125a58eb9b64ed5b7e
JHK922 <apfhdxc@gmail.com>
2023-02-22 20:03:19 +0900

    RentService 대여 부분, 반납 부분 현재 대여상태를 확인 후 대여할 수 있도록 수정 중복 대여, 반납이 이루어지지 않도록 코드 수정
    RentController
    대여 부분에 리턴값 변경,
    코드 정리

--- Library/src/main/java/com/project/library/service/RentService.java
+++ Library/src/main/java/com/project/library/service/RentService.java
@@ -2,38 +2,53 @@ package com.project.library.service;
 
 import com.project.library.dto.request.RentRequest;
 import com.project.library.dto.response.RentResponse;
+import com.project.library.mapper.BookMapper;
 import com.project.library.mapper.RentMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class RentService {
 
     private final RentMapper rentMapper;
+    private final BookMapper bookMapper;
 
     /**
      * 대여 (정보 저장)
+     * book의 status 값을 확인하여 대여가능 상태 일때만 대여가 이루어짐
      * @param request
-     * @return
      */
-    public RentResponse saveRent(RentRequest request) {
-        rentMapper.saveRent(request);
-        rentMapper.updateBookStatusFalse(request); // 대여불가으로 update
-        return rentMapper.findById(request.getId());
+    public void saveRent(RentRequest request) {
+        String status = bookMapper.findByBookId(request.getBookId()).getStatus();
+        if (status.equals("대여불가")) {
+            System.out.println("현재 대여중입니다.");
+        } else if (status.equals("대여가능")) {
+            rentMapper.saveRent(request);
+            rentMapper.updateBookStatusFalse(request); // 대여불가으로 update
+            System.out.println("대여완료");
+        }
     }
 
     /**
      * 반납 (정보 저장)
+     * book의 status 값을 확인하여 대여불가 상태 일때만 반납이 이루어짐
      * @param id - PK
      * @return
      */
-    public RentResponse updateReturn(Long id) {
-        rentMapper.updateReturn(id);
-        rentMapper.updateBookStatusTrue(id); // 대여가능으로 update
-        return rentMapper.findById(id);
+    public void updateReturn(Long id) {
+        String status = rentMapper.findById(id).getStatus();
+        if (status.equals("대여가능")) {
+            System.out.println("현재 대여 가능한 상태입니다.");
+        } else if (status.equals("대여불가")) {
+            rentMapper.updateReturn(id);
+            rentMapper.updateBookStatusTrue(id); // 대여가능으로 update
+            System.out.println("반납완료");
+        }
     }
 
     /**
@@ -43,5 +58,10 @@ public class RentService {
      */
     public RentResponse findById(Long id) {
         return rentMapper.findById(id);
+    }
+
+    public String findStatus(RentRequest request) {
+        String status = bookMapper.findByBookId(request.getBookId()).getStatus();
+        return status;
     }
 }
